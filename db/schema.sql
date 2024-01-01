1725f1cd86ef3069f58497ff1bcaa8913c7640c1
SPCSanti1990 <81482804+SPCSanti1990@users.noreply.github.com>
2024-01-01 11:01:01 -0500

    server.js file added, package.json file created and dependencies added

--- db/schema.sql
+++ db/schema.sql
@@ -21,9 +21,9 @@ CREATE TABLE roles (
 
 --- Create table for employees
 CREATE TABLE employee (
-id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-first_name VARCHAR(255) NOT NULL,
-last_name VARCHAR(255) NOT NULL,
-role_id INT,
-manager_id INT NOT NULL
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    first_name VARCHAR(255) NOT NULL,
+    last_name VARCHAR(255) NOT NULL,
+    role_id INT,
+    manager_id INT NOT NULL
 );
